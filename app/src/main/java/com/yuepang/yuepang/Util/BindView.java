a2aff607ecb370fda539c24331c7086985bc6294
xugh <670225884@qq.com>
2019-03-22 20:59:32 +0800

    修改代码

--- app/src/main/java/com/yuepang/yuepang/Util/BindView.java
+++ app/src/main/java/com/yuepang/yuepang/Util/BindView.java
@@ -6,12 +6,20 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * 自定义注解，用来绑定页面UI控件
  */
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BindView {
+
+    /**
+     *   控件ID
+     */
     int id();
 
+    /**
+     * 是否注册点击事件，默认不注册
+     */
     boolean click() default false;
 }
