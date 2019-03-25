8b91181a2e56abca14cf9106403fbf26504317a6
xugh <670225884@qq.com>
2019-03-25 15:05:59 +0800

    修改代码

--- app/src/main/java/com/yuepang/yuepang/activity/MylikeActivity.java
+++ app/src/main/java/com/yuepang/yuepang/activity/MylikeActivity.java
@@ -1,16 +1,77 @@
 package com.yuepang.yuepang.activity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
 import com.yuepang.yuepang.R;
+import com.yuepang.yuepang.Util.BindView;
 
 /**
- *
  * 我的喜好页面
  */
 
 public class MylikeActivity extends BaseActivity {
+
+    @BindView(id = R.id.ll_cate, click = true)
+    private LinearLayout llcate;// 美食
+
+    @BindView(id = R.id.ll_movie, click = true)
+    private LinearLayout llmovie;// 电影
+
+    @BindView(id = R.id.ll_recreation, click = true)
+    private LinearLayout llrecreation; // 娱乐
+
+    @BindView(id = R.id.ll_group, click = true)
+    private LinearLayout llgroup; // 团购
+
+    @BindView(id = R.id.ll_hotel, click = true)
+    private LinearLayout llhotel; // 酒店
+
+    @BindView(id = R.id.ll_travel, click = true)
+    private LinearLayout lltravel; // 旅行
+
+    @BindView(id = R.id.ll_ktv, click = true)
+    private LinearLayout llktv; // ktv
+
+    @BindView(id = R.id.ll_pet, click = true)
+    private LinearLayout llpet; //宠物
+
+    @BindView(id = R.id.chb_cate)
+    private CheckBox chbCate;// 美食
+
+
+    @BindView(id = R.id.chb_movie)
+    private CheckBox chbMovie;// 电影
+
+    @BindView(id = R.id.chb_recreation)
+    private CheckBox chbRecreation;//娱乐
+
+
+    @BindView(id = R.id.chb_group)
+    private CheckBox chbGroup;// 团购
+
+
+    @BindView(id = R.id.chb_hotel)
+    private CheckBox chbhotel;// 旅馆
+
+    @BindView(id = R.id.ll_travel)
+    private CheckBox chbTravel;
+
+    @BindView(id = R.id.ll_ktv)
+    private CheckBox chbktv;
+
+    @BindView(id = R.id.ll_pet)
+    private CheckBox chbpet;
+
+    @BindView(id = R.id.ll_cate, click = true)
+    private Button btn_sub;
+
+
     @Override
     protected String getMyRTitle() {
-        return "确定";
+        return null;
     }
 
     @Override
@@ -24,7 +85,13 @@ public class MylikeActivity extends BaseActivity {
     }
 
     @Override
-    public void clikRt() {
-        finish();
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.ll_cate:
+                chbCate.setChecked(chbCate.isChecked()? false:true);
+                break;
+
+        }
     }
 }
